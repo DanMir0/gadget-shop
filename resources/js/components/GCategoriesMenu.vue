53612d353d1b0cb1c5cb3bbe62aa33521bd1b841
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-04-06 23:29:44 +0300

    style mobile g-categories

--- resources/js/components/GCategoriesMenu.vue
+++ resources/js/components/GCategoriesMenu.vue
@@ -1,8 +1,10 @@
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
+import router from "@/router/router.js";
 
 const categories = ref([]);
 
+const selected = ref("Все");
 
 async function fetchCategories() {
     try {
@@ -12,6 +14,14 @@ async function fetchCategories() {
         console.log(error)
     }
 }
+
+watch(selected, (newVal) => {
+    if (newVal === "Все") {
+        router.push("/products")
+    } else {
+        router.push(`/products/${newVal}`)
+    }
+})
 
 onMounted(fetchCategories)
 </script>
@@ -27,6 +37,19 @@ onMounted(fetchCategories)
             {{ category.name }}
         </router-link>
     </nav>
+
+    <!--    мобильное меню-->
+    <div class="categories-mobile">
+        <select v-model="selected" class="categories-mobile-selected">
+            <option>Все</option>
+            <option v-for="category in categories"
+                    :key="category.id"
+                    :value="category.slug">
+                {{ category.name }}
+            </option>
+        </select>
+    </div>
+
 </template>
 
 <style scoped>
@@ -43,10 +66,63 @@ onMounted(fetchCategories)
     padding: 19px 61px;
     border: 1px solid black;
     text-decoration: none;
-
 }
 
 .link:hover {
     background: #f4f4f4;
+}
+
+.categories-mobile {
+    display: flex;
+    justify-content: center;
+}
+
+.categories-mobile-selected {
+    padding: 12px 20px;
+    font-size: 16px;
+    color: #000000;
+    background-color: #ffffff;
+    border: 2px solid #cccccc;
+    border-radius: 8px;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3Csvg viewBox='0 0 140 140' width='12' height='12' xmlns='http://www.w3.org/2000/svg'%3E%3Cpolyline points='20 50 70 100 120 50' stroke='%23000' stroke-width='15' fill='none' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-position: right 12px center;
+    background-size: 12px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    width: 100%;
+    max-width: 320px;
+}
+
+.categories-mobile-selected:focus {
+    outline: none;
+    border-color: #000; /* При фокусе становится более чёткой */
+}
+
+.categories-mobile-selected:hover {
+    border-color: #999;
+    background-color: #f9f9f9;
+}
+
+@media (min-width: 768px) {
+    .categories-mobile {
+        display: none;
+    }
+}
+
+@media (max-width: 767px) {
+    .categories {
+        display: none;
+    }
+}
+
+@media (max-width: 1260px) {
+    .categories {
+        justify-content: center;
+        flex-wrap: wrap;
+    }
 }
 </style>
