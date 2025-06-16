81dd7caa3a36c6d04ff66517cbffee992c6c3ebf
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-06-16 16:13:45 +0300

    The footer is pressed to the bottom of the page

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -5,18 +5,36 @@ const {user, isAuthResolved} = useAuth();
 </script>
 
 <template>
-<div>
+<div class="layout">
     <g-loader v-if="!isAuthResolved"></g-loader>
-    <div v-else>
+    <div v-else class="layout-inner">
         <g-header :isAuthResolved="isAuthResolved"></g-header>
 
-        <g-main> <slot></slot></g-main>
+        <g-main class="main-content"> <slot></slot></g-main>
 
-        <g-footer></g-footer>
+        <g-footer class="footer"></g-footer>
     </div>
 </div>
 </template>
 
 <style scoped>
+.layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
 
+.layout-inner {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+.main-content {
+    flex: 1;
+}
+
+.footer {
+    margin-top: auto;
+}
 </style>
