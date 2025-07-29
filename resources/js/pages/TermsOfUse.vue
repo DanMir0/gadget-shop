4e32f682e57192134fdeaf053463960d71c51a90
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-07-29 20:19:12 +0300

    Translated TermsOfUse.vue

--- resources/js/pages/TermsOfUse.vue
+++ resources/js/pages/TermsOfUse.vue
@@ -1,25 +1,50 @@
 <script setup>
-const termsData = [
-    {
-        title: "Условия сервиса",
-        description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
-    },
-    {
-        title: "Условия доставки",
-        description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
-    },
-    {
-        title: "Условия возврата",
-        description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
-    },
-]
+import {computed, onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
 
+const route = useRoute()
+const termsData = {
+    ru: [
+        {
+            title: "Условия сервиса",
+            description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
+        },
+        {
+            title: "Условия доставки",
+            description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
+        },
+        {
+            title: "Условия возврата",
+            description: "Задача организации, в особенности же курс на социально-ориентированный национальный проект требует от нас системного анализа модели развития! Таким образом, постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа системы масштабного изменения ряда параметров! С другой стороны социально-экономическое развитие напрямую зависит от всесторонне сбалансированных нововведений?"
+        },
+    ],
+    en: [
+        {
+            title: "Terms of Service",
+            description: "The task of the organization, especially the course on the socially-oriented national project requires from us a system analysis of the development model! Thus, the constant information and technical support of our activities requires us to analyze the system of large-scale changes in a number of parameters! On the other hand, the socio-economic development directly depends on comprehensive and balanced innovations?"
+        },
+        {
+            title: "Terms of Delivery",
+            description: "The task of the organization, especially the course on the socially-oriented national project requires from us a system analysis of the development model! Thus, the constant information and technical support of our activities requires us to analyze the system of large-scale changes in a number of parameters! On the other hand, the socio-economic development directly depends on comprehensive and balanced innovations?"
+        },
+        {
+            title: "Terms of Returns",
+            description: "The task of the organization, especially the course on the socially-oriented national project requires from us a system analysis of the development model! Thus, the constant information and technical support of our activities requires us to analyze the system of large-scale changes in a number of parameters! On the other hand, the socio-economic development directly depends on comprehensive and balanced innovations?"
+        }
+    ]
+}
+const langData = computed(() => {
+    return ['ru', 'en'].includes(route.params.lang) ? route.params.lang : 'ru'
+})
+onMounted(() => {
+    langData.value = route.params.lang
+})
 </script>
 
 <template>
     <g-container>
         <div class="terms">
-            <div class="terms-block" v-for="(item, key) in termsData" :key="key">
+            <div class="terms-block" v-for="(item, key) in termsData[langData]" :key="key">
                 <h2 class="terms-title">{{ item.title }}</h2>
                 <p class="terms-description ">{{ item.description }}</p>
             </div>
